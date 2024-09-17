b0b1233ff488a4941deed51ddbfe1663c9048497
NATHAN <nathan@mindsetcloud.net>
2024-09-16 21:09:12 -0300

    Update install.sh

--- KUBERNETES/install.sh
+++ KUBERNETES/install.sh
@@ -1,11 +1,8 @@
 # Clear the terminal screen
 sudo apt update
 sudo apt upgrade -y
-
 # Final message
 clear
 echo $USER > /home/$USER/username
 sudo -i
 su $(cat /home/$(ls /home)/username)
-echo "DOCKER INSTALLATION COMPLETED SUCCESSFULLY"
-docker ps
